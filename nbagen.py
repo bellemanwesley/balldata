62feb5b664449aaee2c978732aaf13e780fb8723
bellemanwesley <bellemanwesley@gmail.com>
2019-11-24 10:41:38 -0800

    Add files via upload

---
+++ nbagen.py
@@ -0,0 +1,40 @@
+from random import random
+from itertools import combinations 
+from operator import itemgetter, attrgetter
+
+game_results = []
+for i in range(30):
+	game_results.append([i, 0, 0])
+
+def game_play(t1, r1, t2, r2):
+	if (r1+r2)*random() < r1:
+		game_results[t1][1] += 1
+		game_results[t2][2] += 1
+	else:
+		game_results[t1][2] += 1
+		game_results[t2][1] += 1
+
+rating = []
+for i in range(6):
+	div_rating = []
+	for j in range(5):
+		div_rating.append([i*5+j,random()])
+	rating.append(div_rating)
+
+for i in rating:
+	div_games = list(combinations(i,2))
+	for j in div_games:
+		for k in range(4):
+			game_play(j[0][0],j[0][1],j[1][0],j[1][1])
+
+conf_1 = rating[0]+rating[1]+rating[2]
+conf_2 = rating[3]+rating[4]+rating[5]
+print conf_1
+print conf_2
+for i in conf_1:
+	for j in conf_2:
+		for k in range(2):
+			game_play(i[0],i[1],j[0],j[1])
+
+for i in game_results:
+	print i
