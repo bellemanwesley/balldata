8f39dd423709acd9521d6a586949fbb710578604
Wesley Belleman <wesley.belleman.2@us.af.mil>
2019-11-24 22:50:11 -0800

    Added comments

--- nbagen.py
+++ nbagen.py
@@ -4,9 +4,10 @@ from operator import itemgetter, attrgetter
 
 game_results = []
 for i in range(30):
-	game_results.append([i, 0, 0])
+	game_results.append([i, 0, 0]) #stored as team num, wins, losses
 
-def game_play(t1, r1, t2, r2):
+#Simulates play between two teams
+def game_play(t1, r1, t2, r2): #t1 - num of team 1, r1 - ranking of t1 0-1, etc.
 	if (r1+r2)*random() < r1:
 		game_results[t1][1] += 1
 		game_results[t2][2] += 1
@@ -18,16 +19,16 @@ rating = []
 for i in range(6):
 	div_rating = []
 	for j in range(5):
-		div_rating.append([i*5+j,random()])
+		div_rating.append([i*5+j,random()]) # ratings for teams are randomly generated and held throughout season
 	rating.append(div_rating)
 
-for i in rating:
+for i in rating: #16 in division games
 	div_games = list(combinations(i,2))
 	for j in div_games:
 		for k in range(4):
-			game_play(j[0][0],j[0][1],j[1][0],j[1][1])
+			game_play(j[0][0],j[0][1],j[1][0],j[1][1]) 
 
-for i in range(2):
+for i in range(2): #3 in conference, out of division games, 6 games missing
 	inter_div = list(combinations(range(3),2))
 	for j in inter_div:
 		for k in range(5):
@@ -39,14 +40,14 @@ for i in range(2):
 
 
 
-conf_1 = rating[0]+rating[1]+rating[2]
+conf_1 = rating[0]+rating[1]+rating[2] #merge divs into conferences
 conf_2 = rating[3]+rating[4]+rating[5]
-for i in conf_1:
+for i in conf_1: #30 out of conference games
 	for j in conf_2:
 		for k in range(2):
 			game_play(i[0],i[1],j[0],j[1])
 
-conf_1_r = sorted(game_results[0:15], key = lambda x: x[2])
+conf_1_r = sorted(game_results[0:15], key = lambda x: x[2]) #sort based on wins
 conf_2_r = sorted(game_results[15:30], key = lambda x: x[2])
 
 for i in conf_1_r:
